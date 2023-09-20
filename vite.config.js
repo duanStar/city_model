f09ab4a4f7d91a527a86d77509adbc50a27317dd
Hongfei Duan <1715106673@qq.com>
2023-09-20 12:02:21 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/city_model/',
-  publicDir: '/city_model/',
+  assetsDir: '/city_model/',
   plugins: [vue()],
 })
